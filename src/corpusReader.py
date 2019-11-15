155a2676f769dcc5727eb070e635d840d2904fdf
BigBossAnwer <salman.anwer.tr@gmail.com>
2019-11-14 22:54:26 -0600

    Wrapped corpusReader to a well behaved func call

--- src/corpusReader.py
+++ src/corpusReader.py
@@ -4,116 +4,147 @@ from pathlib import Path
 
 import pandas as pd
 
-description = (
-    f"Eats project data as specified by the structure found in "
-    f"{Path('data/*-set.txt')}"
-)
-parser = argparse.ArgumentParser(description=description)
-parser.add_argument(
-    "-c",
-    "--corpus_path",
-    help=(
-        str(Path("Path/To/Corpus/*-set.txt"))
-        + ", Default: "
-        + str(Path("data/*-set.txt"))
-    ),
-)
-args = parser.parse_args()
 
-data_files = {"dev": "dev-set.txt", "train": "train-set.txt", "test": "test-set.txt"}
-data_dir = args.corpus_path if args.corpus_path is not None else "data"
+def read_data(which_sets, corpus_path=None, log=False):
+    data_files = {"dev": "dev-set.txt", "train": "train-set.txt", "test": "test-set.txt"}
+    data_dir = corpus_path if corpus_path is not None else "data"
+    data_frames = {}
 
-try:
-    dev_path = Path(data_dir, data_files["dev"])
-    print(f"Reading dev-set from: {dev_path}")
-    dev = []
-    rows = 0
-    with open(dev_path) as dev_fh:
-        next(dev_fh)
-        for line in dev_fh.readlines():
-            rows += 1
-            cleaned = []
-            for item in line.split("\t"):
-                clean = item.split("\n") # Clean hanging new line trailing Gold Tag
-                cleaned.append(clean[0])
-            dev.append(cleaned)
+    if "dev" in which_sets:
+        try:
+            dev_path = Path(data_dir, data_files["dev"])
+            print(f"\nReading dev-set from: {dev_path}")
+            dev = []
+            rows = 0
+            with open(dev_path) as dev_fh:
+                next(dev_fh) # Skip header
+                for line in dev_fh.readlines():
+                    rows += 1
+                    cleaned = []
+                    for item in line.split("\t"):
+                        clean = item.split("\n") # Clean hanging new line trailing Gold Tag
+                        cleaned.append(clean[0])
+                    dev.append(cleaned)
 
-    dev_set = pd.DataFrame(dev, columns=["id", "s1", "s2", "gold"]).astype(
-        dtype={"id": object, "s1": object, "s2": object, "gold": int,}
-    )
+            dev_set = pd.DataFrame(dev, columns=["id", "s1", "s2", "gold"]).astype(
+                dtype={"id": object, "s1": object, "s2": object, "gold": int,}
+            )
+            dev_set = dev_set.dropna()
+            pairs_omitted = rows - dev_set.shape[0]
+            print(f"Dev DF shape: {dev_set.shape}")
+            print(f"Dev Pairs Omitted: {pairs_omitted} = {rows} - {dev_set.shape[0]}")
+            data_frames["dev"] = dev_set
 
-except FileNotFoundError:
-    print(f"Error: No such data file at found at {dev_path}")
+        except FileNotFoundError:
+            print(f"Error: No such data file at found at {dev_path}")
+        except:
+            print("Unexpected error: ", sys.exc_info()[0])
+            raise
 
-dev_set = dev_set.dropna()
-pairs_omitted = rows - dev_set.shape[0]
-print(dev_set.head())
-print(f"Dev DF shape: {dev_set.shape}")
-print(f"Dev Pairs Omitted: {pairs_omitted} = {rows} - {dev_set.shape[0]}")
+    if "train" in which_sets:
+        try:
+            train_path = Path(data_dir, data_files["train"])
+            print(f"\nReading train-set from: {train_path}")
+            train = []
+            rows = 0
+            with open(train_path) as train_fh:
+                next(train_fh) # Skip header
+                for line in train_fh.readlines():
+                    rows += 1
+                    cleaned = []
+                    for item in line.split("\t"):
+                        clean = item.split("\n") # Clean hanging new line trailing Gold Tag
+                        if clean[0]:
+                            cleaned.append(clean[0])
+                    train.append(cleaned)
+            
+            train_set = pd.DataFrame(train, columns=["id", "s1", "s2", "gold"]).astype(
+                dtype={"id": object, "s1": object, "s2": object, "gold": int,}
+            )
+            train_set = train_set.dropna()
+            pairs_omitted = rows - train_set.shape[0]
+            print(f"Train DF shape: {train_set.shape}")
+            print(f"Train Pairs Omitted: {pairs_omitted} = {rows} - {train_set.shape[0]}")
+            data_frames["train"] = train_set
 
-try:
-    train_path = Path(data_dir, data_files["train"])
-    print(f"Reading train-set from: {train_path}")
-    train = []
-    rows = 0
-    with open(train_path) as train_fh:
-        next(train_fh)
-        for line in train_fh.readlines():
-            rows += 1
-            cleaned = []
-            for item in line.split("\t"):
-                clean = item.split("\n") # Clean hanging new line trailing Gold Tag
-                if clean[0]:
-                    cleaned.append(clean[0])
-            train.append(cleaned)
+        except FileNotFoundError:
+            print(f"Error: No such data file at found at {train_path}")
+        except:
+            print("Unexpected error: ", sys.exc_info()[0])
+            raise
+
+    if "test" in which_sets:
+        try:
+            test_path = Path(data_dir, data_files["test"])
+            print(f"\nReading test-set from: {test_path}")
+            test = []
+            rows = 0
+            with open(test_path) as test_fh:
+                next(test_fh) # Skip header
+                for line in test_fh.readlines():
+                    rows += 1
+                    cleaned = []
+                    for item in line.split("\t"):
+                        clean = item.split("\n") # Clean hanging new line trailing Gold Tag
+                        cleaned.append(clean[0])
+                    test.append(cleaned)
+
+            test_set = pd.DataFrame(test, columns=["id", "s1", "s2"]).astype(
+                dtype={"id": object, "s1": object, "s2": object,}
+            )
+            test_set = test_set.dropna()
+            pairs_omitted = rows - test_set.shape[0]
+            print(f"Test DF shape: {test_set.shape}")
+            print(f"Test Pairs Omitted: {pairs_omitted} = {rows} - {test_set.shape[0]}")
+            data_frames["test"] = test_set
+
+        except FileNotFoundError:
+            print(f"Error: No such data file at found at {test_path}")
+        except:
+            print("Unexpected error: ", sys.exc_info()[0])
+            raise
     
-    train_set = pd.DataFrame(train, columns=["id", "s1", "s2", "gold"]).astype(
-        dtype={"id": object, "s1": object, "s2": object, "gold": int,}
+    if log:
+        try:
+            Path("log").mkdir(exist_ok=True)
+            for frame in data_frames.keys():
+                data_frames[frame].to_csv(str(Path("log", frame + ".csv")))
+
+        except IOError:
+            print("Error: Log write failed")
+        except:
+            print("Unexpected error: ", sys.exc_info()[0])
+            raise
+    
+    return data_frames
+
+def main():
+    description = (
+        f"Eats project data as specified by the structure found in "
+        f"{Path('data/*-set.txt')}"
     )
-
-except FileNotFoundError:
-    print(f"Error: No such data file at found at {train_path}")
-
-train_set = train_set.dropna()
-pairs_omitted = rows - train_set.shape[0]
-print(train_set.head())
-print(f"Train DF shape: {train_set.shape}")
-print(f"Train Pairs Omitted: {pairs_omitted} = {rows} - {train_set.shape[0]}")
-
-try:
-    test_path = Path(data_dir, data_files["test"])
-    print(f"\nReading test-set from: {test_path}")
-    test = []
-    rows = 0
-    with open(test_path) as test_fh:
-        next(test_fh)
-        for line in test_fh.readlines():
-            rows += 1
-            cleaned = []
-            for item in line.split("\t"):
-                clean = item.split("\n") # Clean hanging new line trailing Gold Tag
-                cleaned.append(clean[0])
-            test.append(cleaned)
-
-    test_set = pd.DataFrame(test, columns=["id", "s1", "s2"]).astype(
-        dtype={"id": object, "s1": object, "s2": object,}
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        "-c",
+        "--corpus_path",
+        help=(
+            str(Path("Path/To/Corpus/*-set.txt"))
+            + ", Default: "
+            + str(Path("data/*-set.txt"))
+        ),
     )
+    parser.add_argument(
+        "-l",
+        "--log",
+        help=(
+            "Suppress dataframe logging to "
+            + str(Path("out"))
+        ),
+        action="store_true"
+    )
+    args = parser.parse_args()
+    
+    read_data(["dev", "test", "train"], args.corpus_path, args.log)
 
-except FileNotFoundError:
-    print(f"Error: No such data file at found at {test_path}")
-
-test_set = test_set.dropna()
-pairs_omitted = rows - test_set.shape[0]
-print(test_set.head())
-print(f"Test DF shape: {test_set.shape}")
-print(f"Test Pairs Omitted: {pairs_omitted} = {rows} - {test_set.shape[0]}")
-
-Path("out").mkdir(exist_ok=True)
-try:
-    dev_set.to_csv(str(Path("out/dev.csv")))
-    train_set.to_csv(str(Path("out/train.csv")))
-    test_set.to_csv(str(Path("out/test.csv")))
-
-except IOError:
-    print("Error: Log write failed")
-    sys.exit()
+if __name__== "__main__":
+  main()
---
+++ src/corpusReader_ex_usage.py
@@ -0,0 +1,15 @@
+from corpusReader import read_data
+
+# Usage: 
+# read_data(
+#     the_sets_you_want=["dev", "test", "train"], # required
+#     custom_path_to_data=path # optional
+#     log=True/False # optional, logs dataframes to log/*.csv
+#     )
+frames = read_data(["train", "test"])
+dev = read_data(which_sets=["dev"])["dev"] # another access pattern
+train, test = frames["train"], frames["test"]
+
+print(train.head())
+print(test.head())
+print(dev.head())
